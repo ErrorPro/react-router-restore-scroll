1e59101e9a6d64bf674f147ad78019bbd9891bbc
Ven Korolev <mohtaiiihuk@gmail.com>
2017-07-16 15:01:10 +0300

    prop-types

--- modules/RestoreScroll.js
+++ modules/RestoreScroll.js
@@ -1,31 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { findDOMNode } from 'react-dom'
 
-const RestoreScroll = React.createClass({
+class RestoreScroll extends React.Component {
+  static contextTypes = {
+    router: PropTypes.object.isRequired
+  }
 
-  contextTypes: {
-    router: React.PropTypes.object.isRequired
-  },
-
-  propTypes: {
-    scrollKey: React.PropTypes.string.isRequired
-  },
+  static propTypes = {
+    scrollKey: PropTypes.string.isRequired
+  }
 
   componentDidMount() {
     const { registerScroller } = this.context.router.restoreScroll
     registerScroller(this.props.scrollKey, findDOMNode(this))
-  },
+  }
 
   componentWillUnmount() {
     const { unregisterScroller } = this.context.router.restoreScroll
     unregisterScroller(this.props.scrollKey)
-  },
+  }
 
   render() {
     return React.Children.only(this.props.children)
   }
-
-})
+}
 
 export default RestoreScroll
 
--- modules/RestoreWindowScroll.js
+++ modules/RestoreWindowScroll.js
@@ -1,26 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const RestoreWindowScroll = React.createClass({
-
-  propTypes: {
-    restoreWindow: React.PropTypes.func.isRequired,
-    location: React.PropTypes.object.isRequired
-  },
+class RestoreWindowScroll extends React.Component {
+  static propTypes = {
+    restoreWindow: PropTypes.func.isRequired,
+    location: PropTypes.object.isRequired
+  }
 
   componentDidMount() {
     this.props.restoreWindow(this.props.location)
-  },
+  }
 
   componentDidUpdate(prevProps) {
     if (prevProps.location !== this.props.location)
       this.props.restoreWindow(this.props.location)
-  },
+  }
 
   render() {
     return React.Children.only(this.props.children)
   }
 
-})
+}
 
 export default RestoreWindowScroll
 
